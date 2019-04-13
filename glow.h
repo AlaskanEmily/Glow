f465b40f31c9eae284c66f5aafef7dc7bede7ea9
AlaskanEmily <emily@alaskanemily.net>
2019-04-12 17:20:58 -0800

    Update copyright year

--- glow.h
+++ glow.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017-2018 Alaskan Emily, Transnat Games
+/* Copyright (C) 2017-2019 Alaskan Emily, Transnat Games
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
